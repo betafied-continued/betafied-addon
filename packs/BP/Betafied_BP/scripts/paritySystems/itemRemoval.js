1b1256f1db2c6b951b9890ab121c4bf9d42594bd
orangutan <60791542+hmm-monke@users.noreply.github.com>
2025-06-07 12:23:36 +0500

    Update itemRemoval.js

--- packs/BP/Betafied_BP/scripts/paritySystems/itemRemoval.js
+++ packs/BP/Betafied_BP/scripts/paritySystems/itemRemoval.js
@@ -173,7 +173,7 @@ class ItemRestrictionSystem {
       "minecraft:light_block_9", "minecraft:light_block_8", "minecraft:light_block_7",
       "minecraft:light_block_6", "minecraft:light_block_5", "minecraft:light_block_4",
       "minecraft:light_block_3", "minecraft:light_block_2", "minecraft:light_block_1",
-      "minecraft:light_block_0", "minecraft:light_block",
+      "minecraft:light_block_0", "minecraft:light_block", "minecraft:bedrock",
       
       // Music Discs
       "minecraft:music_disc_13", "minecraft:music_disc_cat"
